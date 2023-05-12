18078c00ab9c2e4013e3495fa6a4e0aa5f8617ea
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-05-12 12:13:31 +0300

    Add daisyUI nav

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PublicController::class, 'index']);
-Route::get('/about', [PublicController::class, 'about']);
+
+
+Route::get('/', [PublicController::class, 'index'])->name('public.index');
+Route::get('/about', [PublicController::class, 'about'])->name('public.about');
 
